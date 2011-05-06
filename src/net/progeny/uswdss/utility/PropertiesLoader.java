c1e5282c0432119cfb41a4c24003aa92622cb6ec
Chris Hunt <huntca@gmail.com>
2011-05-06 11:44:30 -0700

    Initial commit

---
+++ src/net/progeny/uswdss/utility/PropertiesLoader.java
@@ -0,0 +1,9 @@
+package net.progeny.uswdss.utility;
+
+public class PropertiesLoader {
+
+  public static void main (String args[]) {
+    System.out.println("Hello World.");
+    System.exit(0);
+  }
+}
