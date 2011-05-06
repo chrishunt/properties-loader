062edaa6810ae2e5e7a7500b1924545043b65023
Chris Hunt <huntca@gmail.com>
2011-05-06 12:19:34 -0700

    Finished v1.

--- src/net/progeny/uswdss/utility/PropertiesLoader.java
+++ src/net/progeny/uswdss/utility/PropertiesLoader.java
@@ -1,9 +1,41 @@
 package net.progeny.uswdss.utility;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.ArrayList;
+
 public class PropertiesLoader {
 
+  private final static String PROPERTIES_FILE = "config.properties";
+
   public static void main (String args[]) {
-    System.out.println("Hello World.");
+    // read properties from PROPERTIES_FILE
+    Properties properties = new Properties();
+    try {
+      FileInputStream file = new FileInputStream(new File(PROPERTIES_FILE));
+      properties.load(file);
+      file.close();
+    } catch (Exception e){
+      System.out.println("Error reading properties file. Exiting.");
+      System.exit(1);
+    }
+    
+    ArrayList<String> flipList = new ArrayList<String>();
+    if (properties.getProperty("flip") != null){
+      String[] ids = properties.getProperty("flip").split(",");
+      for (int i=0; i<ids.length; i++){
+        flipList.add(ids[i].trim());
+      }
+    }
+
+    if (flipList.contains("nsms5")){
+      System.out.println("YES!");
+    } else {
+      System.out.println("NO!");
+    }
+
+    System.out.println("Done.");
     System.exit(0);
   }
 }
